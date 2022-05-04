e499a58682abcffba015d021df50ecf8d497e660
adamrobertg3lzyu <75844145+adamrobertg3lzyu@users.noreply.github.com>
2022-05-04 09:49:57 +0200

    Add files via upload

---
+++ AZ Game 3D/include/draw.h
@@ -0,0 +1,28 @@
+#ifndef INCLUDE_DRAW_H_
+#define INCLUDE_DRAW_H_
+
+#include "model.h"
+
+#ifndef GL_EXT_texture_edge_clamp
+#define GL_CLAMP_TO_EDGE                 0x812F
+#endif
+
+// Draw the content.
+void draw_content(World* world);
+
+// Draws the ground.
+void draw_ground(Field field);
+
+// Draws the room.
+void draw_walls(Field field);
+
+// Draws the help menu
+void draw_help(int texture);
+
+// Draws the doors
+void draw_skulls(int moveX, int moveZ,int degree);
+
+// Draws the chairs
+void draw_tanks(int moveX, int moveZ,int degree);
+
+#endif /* INCLUDE_DRAW_H_ */
